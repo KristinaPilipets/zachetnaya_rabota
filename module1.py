d319ac051474fa7be7992295c608720aa9c25bc9
opilane <opilane@HP-CZC6297BB1>
2022-01-21 14:08:38 +0200

    added a lot of stuff :)

---
+++ module1.py
@@ -0,0 +1,122 @@
+from random import*
+
+def psword_check(psword:str)->bool:
+    """функция вернет True если пароль будет соответствовать всем параметрам
+    """
+    digit="d"
+    alpha="a"
+    upper="e"
+    lower="f"
+    symbl="w"
+    if len(psword)!=12:
+        ans=False
+    else:
+        psword=list(psword)
+        for i in psword:
+            if i.isdigit()== True: #string index out of range
+                digit="True"
+            if i.isalpha()== True:
+                alpha="True"
+            if i.isupper()== True:
+                upper="True"
+            if i.islower()==True:
+                lower="True"
+            if i in [".","_","/","@"]:
+                symbl="True"
+        if digit=="True" and upper=="True" and alpha=="True" and lower=="True" and symbl=="True": 
+            ans=True
+        else:
+            ans=False
+    return ans
+def autopsword()->str:
+    """automaatseltloomatud parool
+    """
+    str0=".,:;!_*-+()/#¤%&"
+    str1 = "0123456789"
+    str2 = "qwertyuiopasdfghjklzxcvbnm"
+    str3 = str2.upper()
+    #print(str3) # 'QWERTYUIOPASDFGHJKLZXCVBNM'
+    str4 = str0+str1+str2+str3 #сплошной текст со всеми str
+    ls = list(str4) #создает список значений каждый символ через запятую ["q","w","1"...и тд]
+    shuffle(ls) #перемешиваем значения
+    psword = "".join([choice(ls) for x in range(12)]) # Извлекаем из списка 12 произвольных значений
+    # Пароль готов
+    return psword
+
+def abcde():
+    oige=0
+    kogus=0
+    #tase 1 +,-,0-100
+    #tase 2 +,-,*,/, -100-100
+    #tase 3 +,-,*,/,//,%,** -200-200
+    if tase==1:
+        pass
+    elif tase==2:
+        kogus=int(input("Ülesandete arv: "))
+        for i in range (1,kogus+1):
+            arv1=randint(-100,100)
+            arv2=randint(-100,100)
+            tehe=randint(1,4)
+            if tehe==1:
+                mark="+"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1+arv2
+            elif tehe==2:
+                mark="-"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1-arv2
+            elif tehe==3:
+                mark="*"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1*arv2
+            else:
+                tehe="/"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1/arv2
+            vastus=int(input())
+            if vastus==vas: oige+=1
+    else:
+        kogus=int(input("Ülesandete arv: "))
+        for i in range (1,kogus+1):
+            arv1=randint(-200,200)
+            arv2=randint(-200,200)
+            tehe=randint(1,7)
+            if tehe==1:
+                mark="+"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1+arv2
+            elif tehe==2:
+                mark="-"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1-arv2
+            elif tehe==3:
+                mark="*"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1*arv2
+            elif tehe==4:
+                mark="//"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1//arv2
+            elif tehe==5:
+                mark="%"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1%arv2
+            elif tehe==6:
+                mark="**"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1**arv2
+            else:
+                tehe="/"
+                print(f"{arv1} {mark} {arv2} =")
+                vas=arv1/arv2
+            vastus=int(input("Vastus= "))
+            if vastus==round(vas): oige+=1
+    if (oige/kogus)*100>=90:
+        hinne="5"
+    elif 75<=(oige/kogus)*100<90:
+        hinne="4"
+    elif 60<=(oige/kogus)*100<75:
+        hinne="3"
+    else:
+        hinne="2"
+    print(f"Kokku oli {kogus} ülesandeid, õigesti oli lahendatud {oige} \n Sinu hinne on {hinne}")
--- zachetnaya_rabota.py
+++ zachetnaya_rabota.py
@@ -1,1 +1,129 @@
+from module1 import*
+from tkinter import*
+from tkinter.messagebox import*
+
+def sign():
+    global passw,loginas
+    f=open('users.txt', 'r')
+    userslist=[]
+    for stroka in f:
+        userslist.append(stroka.strip())
+    f.close()
+    login=loginas.get()
+    if login not in userslist:
+        newwd=Toplevel() #tk()
+        Label(newwd,text="Kasutaja ei ole olema, palun regestreerida",font="Calibri 20").grid(row=0,column=0)
+    else:
+        pswords1 = open('pswords.txt', 'r')
+        pswords=[]
+        for string in pswords1:
+            pswords.append(string.strip())
+        pswords1.close()
+        pswor=passw.get()
+        if pswor not in pswords:
+            neww=Toplevel() #tk()
+            Label(neww,text="Vale parool, proovi uuesti",font="Calibri 20").grid(row=0,column=0)
+        else:
+            if userslist.index(login) != pswords.index(pswor):
+                newwo=Toplevel()
+                Label(newwo,text="Vale parool või kasutaja nimi, proovi uuesti",font="Calibri 20").grid(row=0,column=0)
+            else:
+                mathtest()
+
+def signup():
+    global loginas,passw
+    f=open('users.txt', 'r')
+    users=[]
+    for stroka in f:
+        users.append(stroka.strip())
+    f.close()
+    log=loginas.get()
+    if askyesno("Parool","Kas sa tahad kasutada automaatseltloomatud parool?"):
+        pswordA=autopsword()
+        msg=Toplevel()
+        if log in users or log=="" or log==" ":
+            Label(msg,text="See kasutaja nimi on võetud või vali",font="Calibri 20").grid(row=0,column=0)
+        else:
+            Label(msg,text="sinu parool: "+pswordA+"\n"+"sinu login: "+log,font="Calibri 20").grid(row=0,column=0)
+            with open("users.txt", "a") as user:
+                user.write(log+"\n")
+            with open("pswords.txt", "a") as pswrd:
+                pswrd.write(pswordA + "\n")
+            mathtest()
+
+    else:
+         psword=passw.get()
+         ans=psword_check(psword) #подходит ли пароль
+         if ans != True:
+             no=Toplevel()
+             Label(no,text="Parool ei sobi",font="Calibri 20").grid(row=0,column=0)
+         else:
+            msg=Toplevel()
+            if log in users or log=="" or log==" ":
+                Label(msg,text="See kasutaja nimi on võetud või vali",font="Calibri 20").grid(row=0,column=0)
+            else:
+                Label(msg,text="sinu parool: "+pswordA+"\n"+"sinu login: "+log,font="Calibri 20").grid(row=0,column=0)
+                with open("users.txt", "a") as user:
+                    user.write(log+"\n")
+                with open("pswords.txt", "a") as pswrd:
+                    pswrd.write(psword + "\n")
+
+def mathtest():
+    math=Toplevel()
+    Label(math,text="Vale tase",font="Calibri 26").grid(row=1,column=0,columnspan=3,sticky=N+S+W+E)
+    Label(text="Ülesandete arv: ").grid(row=0,column=0,columnspan=2)
+    kogus=Spinbox(math,from_=1,to=20).grid(row=0,column=1,columnspan=3)
+    kog=kogus.get()
+    tase1=Radiobutton(math,text="tase 1",font="Calibri 20",command=taseuks(kog))
+    tase1.grid(row=2,column=0,sticky=N+S+W+E)
+    tase2=Radiobutton(math,text="tase 2",font="Calibri 20",command=tasekaks(kog))
+    tase2.grid(row=2,column=1,sticky=N+S+W+E)
+    tase3=Radiobutton(math,text="tase 3",font="Calibri 20",command=tasekolm(kog))
+    tase3.grid(row=2,column=2,sticky=N+S+W+E)
+
+def taseuks(a):
+    kog=a
+    taseuksTk=Toplevel()
+    for i in range (1,kog+1):
+        arv1=randint(0,100)
+        arv2=randint(0,100)
+        tehe=randint(1,2)
+        if tehe==1:
+            mark="+"
+            Label(taseuksTk,text=arv1+mark+arv2+ "=").grid(row=0,column=0,columnspan=3)
+            vas=arv1+arv2
+        else:
+            mark="-"
+            Label(taseuksTk,text=arv1+mark+arv2+ "=").grid(row=0,column=0,columnspan=3)
+            vas=arv1-arv2
+        vst=Entry(taseuksTk,row=0,column=0)
+        vastus=vst.get()
+        if vastus==vas: oige+=1
+
+def tasekaks():
+    pass
+
+def tasekolm():
+    pass
+
+
+
+wind=Tk()
+wind.title("login ja mateematika")
+
+Label(wind,text="login: ",fg="black").grid(row=1,column=0,sticky=N+S+W+E)
+loginas=Entry(wind,bg="lightblue")
+loginas.grid(row=1,column=2,columnspan=6,sticky=N+S+W+E)
+Label(wind,text="parool: ").grid(row=2,column=0,sticky=N+S+W+E)
+passw=Entry(wind,bg="lightblue")
+passw.grid(row=2,column=2,columnspan=6,sticky=N+S+W+E)
+pres=Button(wind,text="Sise",command=sign)
+pres.grid(row=3,column=3,columnspan=4,sticky=N+S+W+E)
+
+reg=Button(wind,text="regestreerimine",fg="black",command=signup)
+reg.grid(row=5,column=2,columnspan=6,sticky=N+S+W+E)
+
+wind.mainloop()
+
+
 
